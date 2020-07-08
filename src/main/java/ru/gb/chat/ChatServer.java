39cbed391beca08e56c65228f7d802b4768e0471
Ivan Necheukhin <necheukhin@bpcbt.com>
2020-07-08 22:51:10 +0300

    initial

---
+++ src/main/java/ru/gb/chat/ChatServer.java
@@ -0,0 +1,13 @@
+package ru.gb.chat;
+
+public class ChatServer {
+
+    public void start(int port) {
+        System.out.println("Server started on port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+
+}
