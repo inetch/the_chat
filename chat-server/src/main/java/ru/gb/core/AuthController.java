4a9cfc59ddbd11b57e0452b4a3021652d02ac40d
Ivan Necheukhin <necheukhin@bpcbt.com>
2020-08-01 15:44:06 +0300

    приделал базу SQLite. архитектура приложения, безусловно, хромает: вместо возврата статуса из методов типа DBMain.userLogIn лучше возвращать собственно объект User, а всю обработку ошибок вынести в работу с исключениями (кидать свои эксепшены). Проверку пароля намеренно сделал на стороне базы - чтобы не хранить в памяти чат-сервера пароли. Схема БД получилась немного большая (7 таблиц), но это на будущее... Хранение credentials в отдельной таблице - тоже на будущее (так как это sensitive data, то её надо хранить в шифрованных местах). Не смог окончательно победить [SQLITE_BUSY]  The database file is locked (database is locked)

--- chat-server/src/main/java/ru/gb/core/AuthController.java
+++ chat-server/src/main/java/ru/gb/core/AuthController.java
@@ -8,26 +8,46 @@ import java.util.HashMap;
 public class AuthController {
 
     HashMap<String, User> users = new HashMap<>();
+    DBMain db = new DBMain("chat-server/chatserver.db");
 
     public void init() {
+        try {
+            db.connect();
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e.getMessage());
+        }
         for (User user : receiveUsers()) {
             users.put(user.getLogin(), user);
         }
     }
 
-    public String getNickname(String login, String password) {
+    public DBMain.actionResult logIn(String login, String password){
+        DBMain.actionResult res = db.userLogIn(login, password);
+        if(res == DBMain.actionResult.SUCCESS){
+            users.get(login).setLastActionId(DBMain.loginAction);
+        }
+        return res;
+    }
+
+    public String getNickname(String login) {
         User user = users.get(login);
-        if (user != null && user.isPasswordCorrect(password)) {
+        if (user != null && user.getLastActionId() == DBMain.loginAction) {
             return user.getNickname();
         }
         return null;
     }
 
     private ArrayList<User> receiveUsers() {
-        ArrayList<User> usersArr = new ArrayList<>();
-        usersArr.add(new User("admin", "admin", "sysroot"));
-        usersArr.add(new User("alex", "123", "alex-st"));
+        ArrayList<User> usersArr = db.receiveUsers();
         return usersArr;
+    }
+
+    public void closeDBConnection(){
+        db.disconnect();
+    }
+
+    public User getUser(String login){
+        return users.get(login);
     }
 
 }
