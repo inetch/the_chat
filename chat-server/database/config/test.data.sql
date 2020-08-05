0911622e1c032e154b5544c8fb0ab0db9e8b7a7d
Ivan Necheukhin <necheukhin@bpcbt.com>
2020-08-06 01:01:09 +0300

    technical commit

---
+++ chat-server/database/config/test.data.sql
@@ -0,0 +1,6 @@
+insert into usr_credential values (1, 'user1', 'user1');
+insert into usr_credential values (2, 'user2', 'user2');
+insert into usr_credential values (3, 'user3', 'user3');
+insert into usr_user values (1, 'BoneCrusher', 0, null);
+insert into usr_user values (2, 'Poopsik1998', 1, null);
+insert into usr_user values (3, 'MegaTROLLER', -1, null);
